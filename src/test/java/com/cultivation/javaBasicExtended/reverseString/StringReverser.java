efa2df51d2f8f5a17b00e7c513b632effd9a36a6
Tian Zhou <tzhou@thoughtworks.com>
2018-09-10 02:43:35 +0800

    implement reverse method of StringReverser class to pass the tests

--- src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
+++ src/test/java/com/cultivation/javaBasicExtended/reverseString/StringReverser.java
@@ -1,13 +1,23 @@
 package com.cultivation.javaBasicExtended.reverseString;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 class StringReverser {
     @SuppressWarnings({"WeakerAccess", "unused"})
     public static String[] reverse(String input) {
         // TODO: please implement the method to pass all the tests.
         // <--start
-        throw new NotImplementedException();
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+        if (input.length() == 0) {
+            return new String[0];
+        }
+
+        String[] strings = input.split(" ");
+        String[] res = new String[strings.length];
+        for (int i = strings.length - 1, j = 0; i >= 0; i--) {
+            res[j++] = strings[i];
+        }
+        return res;
         // --end-->
     }
 }
